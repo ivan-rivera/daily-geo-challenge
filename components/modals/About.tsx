fe064f9213f186cd98f17b921b8f728411a05ce6
ivan <ivan.s.rivera@gmail.com>
2022-12-19 16:59:55 +1300

    more apostrophes

--- components/modals/About.tsx
+++ components/modals/About.tsx
@@ -10,10 +10,10 @@ function ModalData() {
     <Text>
       This app was authored by a web development hobbyist for the sake of public
       entertainment and education. With that in mind, the app is free (and will
-      always be free) and without ads. However, given that it's a hobby project,
-      it's not perfect. If you find any bugs or have any suggestions, please
-      feel free to either contact me or raise an issue on GitHub. You may also
-      like to check out the Troubleshooting section.
+      always be free) and without ads. However, given that it&apos;s a hobby
+      project, it&apos;s not perfect. If you find any bugs or have any
+      suggestions, please feel free to either contact me or raise an issue on
+      GitHub. You may also like to check out the Troubleshooting section.
     </Text>
   );
 }
