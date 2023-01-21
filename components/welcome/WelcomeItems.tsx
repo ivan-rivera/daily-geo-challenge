8045e62420a238136f722f2e36e6eaff8bc37f33
ivan <ivan.s.rivera@gmail.com>
2023-01-21 17:09:58 +1300

    refactored delay values

--- components/welcome/WelcomeItems.tsx
+++ components/welcome/WelcomeItems.tsx
@@ -1,17 +1,23 @@
 import { IconChecks, IconGlobe, IconShare } from "@tabler/icons"
-import WelcomeItem, { WelcomeItemProps } from "./WelcomeItem"
+import WelcomeItem from "./WelcomeItem"
 import getConfig from "next/config"
 import { CardBody, VStack } from "@chakra-ui/react"
+import { BOX_BORDER_RADIUS } from "../../lib/constants"
 
 const { publicRuntimeConfig } = getConfig()
 
-const welcomeItems: WelcomeItemProps[] = [
-  { icon: IconGlobe, text: "Test your knowledge of geography", delay: 0.25 },
-  { icon: IconShare, text: "Challenge your friends", delay: 0.5 },
+const welcomeItems = [
+  {
+    icon: IconGlobe,
+    text: "Test your knowledge of geography",
+  },
+  {
+    icon: IconShare,
+    text: "Challenge your friends",
+  },
   {
     icon: IconChecks,
     text: `${publicRuntimeConfig.questions} daily questions`,
-    delay: 0.75,
   },
 ]
 
@@ -26,16 +32,15 @@ export default function WelcomeItems() {
       color="tertiary"
       borderBottom="1px"
       borderColor="tertiary"
-      borderTopLeftRadius={15}
-      borderTopRightRadius={15}
+      borderTopRadius={BOX_BORDER_RADIUS}
     >
       <VStack align="self-start">
-        {welcomeItems.map((item) => (
+        {welcomeItems.map((item, index) => (
           <WelcomeItem
             key={item.text}
             icon={item.icon}
             text={item.text}
-            delay={item.delay}
+            delay={(index + 1) * 0.25}
           />
         ))}
       </VStack>
