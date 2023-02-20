6cc732ecc5899bb95518e3336e2323810b23706a
ivan <ivan.s.rivera@gmail.com>
2023-02-20 10:15:41 +0100

    removed image URL tests

--- tests/data/bank.test.ts
+++ tests/data/bank.test.ts
@@ -2,7 +2,6 @@
  * @jest-environment node
  */
 
-import axios from "axios"
 import { getDuplicates } from "../support"
 import data from "../../lib/questions/bank"
 
@@ -12,20 +11,5 @@ describe("options must be unique", () => {
       const duplicates = getDuplicates(Object.values(q.data))
       expect(duplicates).toHaveLength(0)
     })
-  })
-})
-
-describe("images must be loaded correctly", () => {
-  data.forEach((q) => {
-    if (q.image) {
-      it(`should have a valid image for question ${q.id}`, () => {
-        Object.values(q.data).forEach(async (value) => {
-          if (typeof value === "string") {
-            const response = await axios.get(value)
-            expect(response.status).toBe(200)
-          }
-        })
-      })
-    }
   })
 })
